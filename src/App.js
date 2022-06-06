c0516439bbd0d33b83f0034d4cd70c289482f1db
TruongNguyen <truongnv65@fpt.com.vn>
2022-06-06 16:52:16 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import HeartImage from "./resources/heart.webp";
+
+function App() {
+  return (
+    <>
+      <h1 className="App">Ở đây khum có gì hết. Chỉ có một chút tình iu</h1>
+      <img src={HeartImage} width="100%"></img>
+    </>
+  );
+}
+
+export default App;
